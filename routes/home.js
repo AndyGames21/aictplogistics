f5f28bd1e64ad1ce5dfadab6619a1112d179ecb0
AndrewTheDev <andy.bad.gaming@gmail.com>
2025-09-27 10:24:44 +0100

    Tried Fixing Homepage

--- routes/home.js
+++ routes/home.js
@@ -27,7 +27,7 @@ router.get("/", async (req, res) => {
   res.render("dashboard", { 
     greeting: getGreeting(),
     user, 
-    bookings,
+    bookings, 
     services,
     slides,
     title: "Dashboard | AICTP Logistics LTD",
